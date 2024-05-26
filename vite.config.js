19bd5e527a118b40998cc99dbf4e7d065ef15ce4
simiariyo <simiariyo@gmail.com>
2024-05-26 11:32:10 +0100

    just something

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-const isProduction = process.env.NODE_ENV === 'production';
+// const isProduction = process.env.NODE_ENV === 'production';
 
-// Set the base path conditionally
-const base = isProduction ? '/victoria-portfolio/' : '/';
+// // Set the base path conditionally
+// const base = isProduction ? '/victoria-portfolio/' : '/';
 export default defineConfig({
   plugins: [react()],
-  base,
+  base: '/victoria-portfolio/',
   build: {
     outDir: 'dist',
   },
